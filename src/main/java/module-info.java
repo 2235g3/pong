4bfc4b9ecf9bffcd805d6c2d6a9468eb34a8534d
jacob <cashinhand9033@gmail.com>
2023-08-22 17:29:40 +0100

    Resized

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,9 @@
 module gh.nijd.pong {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                                requires com.almasb.fxgl.all;
-    
+
+    requires com.almasb.fxgl.all;
+
     opens gh.nijd.pong to javafx.fxml;
     exports gh.nijd.pong;
 }
