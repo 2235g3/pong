412b126f93f6a3e90b45ecdd45440cf9967241fc
jacob <cashinhand9033@gmail.com>
2023-08-24 20:55:37 +0100

    Added: - GameLoop - Keybinds

--- src/main/java/gh/nijd/pong/GameController.java
+++ src/main/java/gh/nijd/pong/GameController.java
@@ -1,7 +1,13 @@
 package gh.nijd.pong;
 
+import javafx.animation.AnimationTimer;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Line;
 
@@ -17,10 +23,65 @@ public class GameController {
     @FXML
     Line rightEnd;
 
+    public boolean up = false, down = false;
+
     public void initialize() {
         Player p1 = new Player("Player One", "p1");
         Player p2 = new Player("Player Two", "p2");
+
+        EventHandler<KeyEvent> pressedEventHandler = new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                switch (keyEvent.getCode()) {
+                    case S:
+                        System.out.println("S Pressed");
+                        down = true;
+                        break;
+                    case W:
+                        System.out.println("Test");
+                        up = true;
+                        break;
+                }
+            }
+        };
+
+        EventHandler<KeyEvent> releasedEventHandler = new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                switch (keyEvent.getCode()) {
+                    case S:
+                        System.out.println("S Released");
+                        down = false;
+                        break;
+                    case W:
+                        System.out.println("W Released");
+                        up = false;
+                        break;
+                }
+            }
+        };
+
+        AnimationTimer gameLoop = new AnimationTimer() {
+            @Override
+            public void handle(long l) {
+                if (down) {
+                    p1.yPos += 15;
+                    p1.r.setY(p1.yPos);
+                }
+                else if (up) {
+                    p1.yPos -= 15;
+                    p1.r.setY(p1.yPos);
+                }
+            }
+        };
+
+
+
         p1.initRectangle(gamePane);
         p2.initRectangle(gamePane);
+
+        //gamePane.getScene().setOnKeyPressed(pressedEventHandler);
+        //gamePane.getScene().setOnKeyReleased(releasedEventHandler);
+        gameLoop.start();
     }
 }
--- src/main/java/gh/nijd/pong/Player.java
+++ src/main/java/gh/nijd/pong/Player.java
@@ -1,5 +1,8 @@
 package gh.nijd.pong;
 
+import javafx.event.EventHandler;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -30,14 +33,37 @@ public class Player {
     }
 
     public void initRectangle(Pane gamePane) {
+        rectangleDimensions();
+        r = new Rectangle(xPos, yPos, width, height);
+        r.setFill(fillColor);
+        EventHandler<KeyEvent> eventHandler = new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                switch (keyEvent.getCode()) {
+                    case S:
+                        System.out.println("Text");
+                        yPos += 15;
+                        r.setY(yPos);
+                    case W:
+                        System.out.println("Test");
+                        yPos -= 15;
+                        r.setY(yPos);
+                }
+            }
+        };
+        r.addEventHandler(KeyEvent.KEY_PRESSED, eventHandler);
+        gamePane.getChildren().add(r);
+    }
+
+    private void rectangleDimensions() {
         if (playerType.equals("p2")) {
             xPos = 1480 - width;
         }
 
         yPos = (750 / 2) - (height / 2);
+    }
 
-        r = new Rectangle(xPos, yPos, width, height);
-        r.setFill(fillColor);
-        gamePane.getChildren().add(r);
+    private void rectangleEvents() {
+
     }
 }
