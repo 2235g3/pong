7417bdaf57e306a45f9684b0f60bf38e2c8d7865
jacob <cashinhand9033@gmail.com>
2023-08-25 23:12:54 +0100

    Added: - Game Icon

--- src/main/java/gh/nijd/pong/ApplicationStart.java
+++ src/main/java/gh/nijd/pong/ApplicationStart.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,6 +16,7 @@ public class ApplicationStart extends Application {
         Scene scene = new Scene(fxmlLoader.load(), 1500, 750);
         stage.setResizable(false);
         stage.setTitle("Pong Game");
+        stage.getIcons().add(new Image((getClass().getResourceAsStream("images/pong.png"))));
         stage.setScene(scene);
         stage.show();
     }
